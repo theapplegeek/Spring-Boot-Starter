5d9235f8decfdb5392f81cb49b56c0b1f732604d
Weibin Xu <weibin.xu@agmsolutions.net>
2024-06-23 02:10:44 +0200

    Initial commit

---
+++ src/main/java/it/theapplegeek/spring_starter_pack/SpringStarterPackApplication.java
@@ -0,0 +1,13 @@
+package it.theapplegeek.spring_starter_pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStarterPackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStarterPackApplication.class, args);
+	}
+
+}
