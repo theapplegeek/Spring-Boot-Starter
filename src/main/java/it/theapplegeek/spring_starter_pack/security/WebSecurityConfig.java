4683f54729137fde339e225b9e4f17998fc9f722
Weibin Xu <w.xu@namirial.com>
2025-11-09 08:26:38 +0100

    Fix CORS Policy Blocked in browser

--- src/main/java/it/theapplegeek/spring_starter_pack/security/WebSecurityConfig.java
+++ src/main/java/it/theapplegeek/spring_starter_pack/security/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -24,7 +25,8 @@ public class WebSecurityConfig {
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http.csrf(AbstractHttpConfigurer::disable)
+    http.cors(Customizer.withDefaults())
+        .csrf(AbstractHttpConfigurer::disable)
         .authorizeHttpRequests(
             req ->
                 req.requestMatchers("/api/auth/**")
